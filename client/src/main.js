e56f3d26cbb9dd7bff3def3d39609ec1d88ee710
Bohdan Pusanuk <pusanukbr@live.ru>
2018-03-03 14:09:22 +0200

    structure only scss

--- client/src/main.js
+++ client/src/main.js
@@ -1,30 +1,9 @@
 // import Vue from 'vue'
-import Vue from 'vue/dist/vue.js'
-import App from './components/App.vue'
+import Vue from 'vue'
 import Start from './Start.vue'
-import HelloWorld from './components/HelloWorld.vue'
-import VueRouter from 'vue-router'
-import wcom from 'vue-router'
+import router from './router'
 
 Vue.config.productionTip = false
-
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-	routes: [
-	{ 
-		path: '/App',
-		name: "pageOne",
-		component: App
-	},
-	{ 
-		path: '/Helloworld',
-		name: "pageTwo",
-		component: HelloWorld
-	}
-	],
-	mode: 'history'
-})
 
 new Vue({
   el: "#app",
